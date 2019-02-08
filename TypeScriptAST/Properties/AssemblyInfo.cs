b2e424f32c4577be2792045974a4a7cbb885dbba
Oleg Zudov <tocsharp@gmail.com>
2019-02-08 17:43:56 +0300

    nuget 1.0.2

--- TypeScriptAST/Properties/AssemblyInfo.cs
+++ TypeScriptAST/Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("7728b98c-9e88-4e6d-8ecd-d51ded278ac2")]
 
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
